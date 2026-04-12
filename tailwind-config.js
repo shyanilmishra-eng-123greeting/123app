e2fa37868c061a9d3ad928591e2083c2a5daa16d
Shyanil mishra <shyanilmishra@Shyanils-MacBook-Air.local>
2026-04-12 15:39:29 +0530

    first commit

---
+++ tailwind-config.js
@@ -0,0 +1,67 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            "colors": {
+                "tertiary-fixed": "#ffddb4",
+                "surface-container-lowest": "#ffffff",
+                "surface-container-high": "#eae8e6",
+                "on-secondary-fixed": "#121c27",
+                "error-container": "#ffdad6",
+                "surface-container": "#efedec",
+                "surface-variant": "#e4e2e0",
+                "on-surface-variant": "#5d3f3c",
+                "on-primary-container": "#fff3f2",
+                "on-primary": "#ffffff",
+                "on-tertiary-fixed-variant": "#633f00",
+                "on-tertiary-container": "#fff4ea",
+                "on-primary-fixed-variant": "#93000f",
+                "inverse-surface": "#30302f",
+                "surface-bright": "#fbf9f7",
+                "outline": "#916f6b",
+                "on-error": "#ffffff",
+                "secondary-fixed": "#d9e3f2",
+                "primary-fixed": "#ffdad6",
+                "tertiary-container": "#9b6500",
+                "surface-dim": "#dbdad8",
+                "primary-container": "#dc1f26",
+                "tertiary-fixed-dim": "#ffb955",
+                "on-secondary-container": "#5b6571",
+                "surface-tint": "#c00017",
+                "on-primary-fixed": "#410003",
+                "background": "#fbf9f7",
+                "secondary": "#555f6b",
+                "on-secondary": "#ffffff",
+                "on-tertiary-fixed": "#291800",
+                "outline-variant": "#e6bdb9",
+                "on-tertiary": "#ffffff",
+                "secondary-container": "#d9e3f2",
+                "surface": "#fbf9f7",
+                "surface-container-highest": "#e4e2e0",
+                "on-surface": "#1b1c1b",
+                "on-background": "#1b1c1b",
+                "primary": "#b40015",
+                "secondary-fixed-dim": "#bdc7d5",
+                "inverse-primary": "#ffb3ac",
+                "error": "#ba1a1a",
+                "on-error-container": "#93000a",
+                "primary-fixed-dim": "#ffb3ac",
+                "tertiary": "#7a4f00",
+                "inverse-on-surface": "#f2f0ee",
+                "surface-container-low": "#f5f3f1",
+                "on-secondary-fixed-variant": "#3e4853"
+            },
+            "borderRadius": {
+                "DEFAULT": "1rem",
+                "lg": "2rem",
+                "xl": "3rem",
+                "full": "9999px"
+            },
+            "fontFamily": {
+                "headline": ["Newsreader", "serif"],
+                "body": ["Plus Jakarta Sans", "sans-serif"],
+                "label": ["Plus Jakarta Sans", "sans-serif"]
+            }
+        }
+    }
+};
